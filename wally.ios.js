becb2f2cde4b96dee3fff4c63eff479e9e66cbd9
Marco Argentieri <marco@vulpem.com>
2019-02-24 15:36:09 +0100

    js ios stub

--- wally.ios.js
+++ wally.ios.js
@@ -1,10 +1,5 @@
 /**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- * Stub of wally for Android.
+ * Stub of wally for iOS.
  *
  * @format
  * @flow strict-local
@@ -12,7 +7,7 @@
 
 'use strict';
 
-const Nativewally = require('NativeModules').wally;
+const Nativewally = require('react-native').NativeModules;
 
 /**
  * High-level docs for the wally iOS API can be written here.
